fae9c51ba2a9d0970215f36444c3cf4cf55a4b31
johmarjac <johannesmjacobs@gmail.com>
2018-03-05 10:57:18 +0100

    Added Sprite class

---
+++ MonoGame.Additions/Graphics/Sprite.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Additions.Graphics
+{
+    public class Sprite : ITransform2D
+    {
+        public Sprite(Texture2D texture)
+        {
+            Texture = texture;
+            IsVisible = true;
+        }
+
+        public Vector2 Position { get; set; }
+        public Vector2 Scalation { get; set; }
+        public float Rotation { get; set; }
+        public Texture2D Texture { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
