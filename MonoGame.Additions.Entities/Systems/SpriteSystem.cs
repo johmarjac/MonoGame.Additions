60e6f2a8500aa637f16c9c6f793136513c8df0f1
johmarjac <johannesmjacobs@gmail.com>
2018-03-05 10:58:34 +0100

    Added basic Sprite System

---
+++ MonoGame.Additions.Entities/Systems/SpriteSystem.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Additions.Entities.Attributes;
+using MonoGame.Additions.Entities.Components;
+
+namespace MonoGame.Additions.Entities.Systems
+{
+    [RequiredComponents(typeof(SpriteBatchComponent), typeof(SpriteComponent))]
+    public class SpriteSystem : ComponentSystem
+    {
+        public override void DrawEntity(Entity entity, GameTime gameTime)
+        {
+            base.DrawEntity(entity, gameTime);
+
+            var batch = entity.GetComponent<SpriteBatchComponent>();
+            var sprites = entity.GetComponents<SpriteComponent>();
+
+            batch.SpriteBatch.Begin();
+
+            foreach (var sprite in sprites)
+            {
+                if (!sprite.Sprite.IsVisible)
+                    continue;
+
+                batch.SpriteBatch.Draw(sprite.Sprite.Texture,
+                    new Rectangle(sprite.Sprite.Position.ToPoint(), sprite.Sprite.Texture.Bounds.Size),
+                    new Rectangle(0, 0, sprite.Sprite.Texture.Width, sprite.Sprite.Texture.Height),
+                    Color.White,
+                    sprite.Sprite.Rotation,
+                    Vector2.Zero, SpriteEffects.None, 1f);
+            }
+
+            batch.SpriteBatch.End();
+        }
+    }
+}
