de813713f2d154636063974f6223042e703f4e8d
johmarjac <johannesmjacobs@gmail.com>
2018-03-14 12:55:40 +0100

    Added Platformer Demo Game

---
+++ PlatformerGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlatformerGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PlatformerGame())
+                game.Run();
+        }
+    }
+}
