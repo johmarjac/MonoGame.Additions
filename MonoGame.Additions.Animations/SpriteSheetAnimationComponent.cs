8cde37d77f874e05940cbea5c9e71e6c4529f34c
johmarjac <johannesmjacobs@gmail.com>
2018-04-17 13:10:17 +0200

    Parts of AnimationSystem

---
+++ MonoGame.Additions.Animations/SpriteSheetAnimationComponent.cs
@@ -0,0 +1,9 @@
+﻿using MonoGame.Additions.Entities;
+
+namespace MonoGame.Additions.Animations
+{
+    public class SpriteSheetAnimationComponent : EntityComponent
+    {
+        public SpriteSheetAnimations Animations { get; set; }
+    }
+}
---
+++ MonoGame.Additions.Animations/Systems/SpriteSheetAnimationSystem.cs
@@ -0,0 +1,13 @@
+﻿using MonoGame.Additions.Entities;
+using MonoGame.Additions.Entities.Attributes;
+using MonoGame.Additions.Entities.Components;
+
+namespace MonoGame.Additions.Animations.Systems
+{
+    [ComponentSystem]
+    [RequiredComponents(typeof(SpriteSheetAnimationComponent), typeof(TransformComponent))]
+    public class SpriteSheetAnimationSystem : ComponentSystem
+    {
+
+    }
+}
